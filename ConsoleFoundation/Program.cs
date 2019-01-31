b89d61f7e8e1ddd5e792132c5a3d3f1b520641eb
free <devqj@qq.com>
2019-01-31 10:22:12 -0400

    init

---
+++ ConsoleFoundation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleFoundation
+{
+    /// <summary>
+    /// 控制台程序的基础框架
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
